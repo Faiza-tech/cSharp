ffb7ebf426e6ec8ad7ee098d8fd10ccab1673c7e
Faiza <faraz_ul_islam@yahoo.com>
2024-10-08 13:10:56 +0100

    Switch statment

--- switchStatment/Program.cs
+++ switchStatment/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+int employeeLevel = 200;
+string employeeName = "John";
+string title = "";
+
+switch(employeeLevel){
+
+    case 100:
+    title = "Junior Associate";
+    break;
+
+    case 200:
+    title = "Senior Associate";
+    break;
+
+    case 300:
+    title = "Manager";
+    break;
+
+    case 400:
+    title = "Senior Manager";
+    break;
+
+    default:
+    title = "Associate";
+    break;
+};
+
+Console.WriteLine($"{employeeName}: level is {employeeLevel}, and he is {title}");
